ffccc99dd4d11600a5cc02de5d1a904ba13684d0
bloc97 <bloc97@users.noreply.github.com>
2017-10-01 19:11:04 -0400

    updated license and javadoc

--- uVector/src/main/java/com/bloc97/uvector/Vector2.java
+++ uVector/src/main/java/com/bloc97/uvector/Vector2.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2017 Bowen Peng.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package com.bloc97.uvector;
 
@@ -100,6 +118,11 @@ public class Vector2 implements Vector<Vector2> {
         return set(1, d);
     }
     
+    /**
+     * Rotates the vector by an angle value in radians
+     * @param angle Angle value in radians
+     * @return The same vector for method chaining
+     */
     public Vector2 rotate(double angle) {
         double s = Math.sin(angle);
         double c = Math.cos(angle);
